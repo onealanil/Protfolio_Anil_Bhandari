ce9ec0d77a8745725e4db6aaf0519014c60ade26
Anil <khalifaanil84@gmail.com>
2024-08-16 21:08:03 +0545

    flask

--- src/components/MobHeader.tsx
+++ src/components/MobHeader.tsx
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 import SearchBar from "./SearchBar";
+import { FlaskConical } from "lucide-react";
 
 function MobHeader() {
   return (
-    <header
-      className={` ${"bg-white py-3"}`}
-    >
+    <header className="bg-white py-3">
       <div className="flex flex-col items-center justify-between w-full px-6">
         <div className="w-full flex justify-between items-center mb-3">
-          <div className="w-10"></div> {/* Spacer */}
-          <div
-            className={`relative ${"w-[10.5rem] h-[4.5rem]"} transition-all duration-300`}
-          >
-            <Image
-              src="/google.png"
-              alt="Google"
-              layout="fill"
-              objectFit="contain"
-              priority
-            />
+          <div className="flex items-center justify-center w-10">
+            <FlaskConical size={28} className="text-gray-500" />
           </div>
-          <button className="p-2 hover:bg-gray-100 rounded-full">
+          <div className="flex-grow flex justify-center">
+            <div className="relative w-[10.5rem] h-[4.5rem]">
+              <Image
+                src="/google.png"
+                alt="Google"
+                layout="fill"
+                objectFit="contain"
+                priority
+              />
+            </div>
+          </div>
+          <button className="w-10 h-10 p-1 hover:bg-gray-100 rounded-full overflow-hidden">
             <Image
               src="https://res.cloudinary.com/dcnm2ql9y/image/upload/v1718208300/z3exzrbgzbiknafr6l5f.jpg"
               alt="User"
