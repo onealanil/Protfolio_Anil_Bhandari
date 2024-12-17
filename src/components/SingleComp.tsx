0c70c576b03b74b52cb4d871b2aa8dd24d3839d2
Anil <khalifaanil84@gmail.com>
2024-12-17 14:52:12 +0545

    changes-width

--- src/components/SingleComp.tsx
+++ src/components/SingleComp.tsx
@@ -19,7 +19,7 @@ const SingleComp = ({ onClose, singleData }: any) => {
         singleData?.name === "LinkedIn - Anil Bhandari"
           ? "h-auto max-h-[90vh] lg:max-h-[80vh]"
           : "h-[30rem] lg:h-[40rem]"
-      } lg:max-w-lg lg:mx-auto w-[100%] bg-white shadow-lg rounded-lg overflow-hidden flex flex-col`}
+      } lg:max-w-lg lg:mx-auto w-[90%] bg-white shadow-lg rounded-lg overflow-hidden flex flex-col`}
     >
       {/* Header */}
       <div className="flex items-center justify-between p-4 bg-gray-100">
